773e641d5a6a0093fa2a70917463eea74fd842c7
Katie Holly <git@meo.ws>
2018-05-24 10:15:31 +0200

    Add ipset health checks

--- files/healthcheck.sh
+++ files/healthcheck.sh
@@ -6,4 +6,10 @@ birdc show status &> /dev/null || exit 1
 # Check if iptables module is responsive
 iptables -nL FORWARD &> /dev/null || exit 1
 
+# Check if ipset lists exist
+ipset save managed4 &> /dev/null || exit 1
+ipset save managed6 &> /dev/null || exit 1
+ipset save v4 &> /dev/null || exit 1
+ipset save v6 &> /dev/null || exit 1
+
 exit 0
